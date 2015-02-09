4b891982f8d484d2973ddd5f9aba3de20b464a66
hadong <hadong@9135BX1.ap.Acxiom.net>
2015-02-09 10:17:31 +0800

    init

--- src/main/java/com/minws/wechat/frame/sdk/qiniu/QiniuKit.java
+++ src/main/java/com/minws/wechat/frame/sdk/qiniu/QiniuKit.java
@@ -5,9 +5,12 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.qiniu.api.fop.ImageView;
 import com.qiniu.api.io.IoApi;
 import com.qiniu.api.io.PutExtra;
 import com.qiniu.api.io.PutRet;
+import com.qiniu.api.net.CallRet;
+import com.qiniu.api.rs.EntryPath;
 import com.qiniu.api.rs.RSClient;
 import com.qiniu.api.rsf.ListItem;
 import com.qiniu.api.rsf.ListPrefixRet;
@@ -20,28 +23,72 @@ public class QiniuKit {
 		mConfig = config;
 	}
 
+	/**
+	 * 上传文件
+	 * 
+	 * @param bucketName
+	 * @param filePath
+	 * @param is
+	 * @return
+	 */
 	public static PutRet put(String bucketName, String filePath, InputStream is) {
 		String token = mConfig.getToken(bucketName);
 		return IoApi.Put(token, filePath, is, new PutExtra());
 	}
 
+	/**
+	 * 上传文件
+	 * 
+	 * @param bucketName
+	 * @param filePath
+	 * @param file
+	 * @return
+	 */
 	public static PutRet put(String bucketName, String filePath, File file) {
 		String token = mConfig.getToken(bucketName);
 		return IoApi.putFile(token, filePath, file, new PutExtra());
 	}
 
+	/**
+	 * 批量获取文件列表
+	 * 
+	 * @param bucketName
+	 * @return
+	 */
 	public static List<ListItem> list(String bucketName) {
 		return list(bucketName, 0);
 	}
 
+	/**
+	 * 批量获取文件列表
+	 * 
+	 * @param bucketName
+	 * @param count
+	 * @return
+	 */
 	public static List<ListItem> list(String bucketName, int count) {
 		return list(bucketName, "", count);
 	}
 
+	/**
+	 * 批量获取文件列表
+	 * 
+	 * @param bucketName
+	 * @param directoryPath
+	 * @return
+	 */
 	public static List<ListItem> list(String bucketName, String directoryPath) {
 		return list(bucketName, directoryPath, 0);
 	}
 
+	/**
+	 * 批量获取文件列表
+	 * 
+	 * @param bucketName
+	 * @param directoryPath
+	 * @param count
+	 * @return
+	 */
 	public static List<ListItem> list(String bucketName, String directoryPath, int count) {
 		if (directoryPath == null) {
 			directoryPath = "";
@@ -61,8 +108,45 @@ public class QiniuKit {
 		return items;
 	}
 
+	/**
+	 * 删除文件
+	 * 
+	 * @param bucketName
+	 * @param filePath
+	 */
 	public static void remove(String bucketName, String filePath) {
 		RSClient client = mConfig.getRSClient();
 		client.delete(bucketName, filePath);
+	}
+
+	/**
+	 * 批量删除文件
+	 * 
+	 * @param entryPath
+	 */
+	public static void batchRemove(List<EntryPath> entryPath) {
+		RSClient client = mConfig.getRSClient();
+		client.batchDelete(entryPath);
+	}
+
+	/**
+	 * 预览文件 format = jpg
+	 * 
+	 * @param url
+	 * @param height
+	 * @param width
+	 * @param quality
+	 * @param mode
+	 * @param format
+	 * @return
+	 */
+	public static CallRet preView(String url, int height, int width, int quality, int mode, String format) {
+		ImageView iv = new ImageView();
+		iv.height = height;
+		iv.width = width;
+		iv.quality = quality;
+		iv.mode = mode;
+		iv.format = format;
+		return iv.call(url);
 	}
 }
